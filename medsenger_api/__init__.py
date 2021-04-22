f248842ebd34f1f08eb22d8cf520cb1d66912d1c
roctbb <roctbb@gmail.com>
2021-04-22 12:24:16 +0300

    project structure

--- medsenger_api/__init__.py
+++ medsenger_api/__init__.py
@@ -1,3 +1,12 @@
+"""
+medsenger_api.
+Python SDK for Medsenger.AI
+"""
+
+__version__ = "0.1"
+__author__ = 'Rostislav Borodin'
+__credits__ = 'TelePat LLC'
+
 import uuid
 from copy import copy
 import requests
---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name='medsenger_api',
+    version='0.1',
+    description='Python SDK for Medsenger.AI',
+    url='https://github.com/roctbb/medsenger_api',
+    author='Rostislav Borodin',
+    author_email='borodin@medsenger.ru',
+    license='BSD 2-clause',
+    packages=['medsenger_api'],
+    install_requires=['requests'],
+
+    classifiers=[
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python :: 3.7',
+    ],
+)
