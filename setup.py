c02c7de0474388d7e1578a98295978cc0a545935
roctbb <roctbb@gmail.com>
2021-12-06 14:30:12 +0300

    0.1.13

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='medsenger_api',
-    version='0.1.12',
+    version='0.1.13',
     description='Python SDK for Medsenger.AI',
     url='https://github.com/roctbb/medsenger_api',
     author='Rostislav Borodin',
