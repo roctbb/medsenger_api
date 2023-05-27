5f352b6264aa62b6ede2c0f4f583ff87105fe330
roctbb <roctbb@gmail.com>
2023-05-27 11:34:53 +0300

    0.1.49 add categories cache

--- setup.py
+++ setup.py
@@ -2,18 +2,18 @@ from setuptools import setup
 
 setup(
     name='medsenger_api',
-    version='0.1.48',
+    version='0.1.49',
     description='Python SDK for Medsenger.AI',
     url='https://github.com/roctbb/medsenger_api',
     author='Rostislav Borodin',
     author_email='borodin@medsenger.ru',
     license='BSD 2-clause',
     packages=['medsenger_api'],
-    install_requires=['requests', 'python-magic'],
-    package_data={'': ['*']},
-    include_package_data=True,
+    install_requires=['requests', 'python-magic', 'grpcio', 'grpcio-tools', 'sentry-sdk'],
+    package_data={'protocol': ['*']},
     classifiers=[
         'License :: OSI Approved :: BSD License',
         'Programming Language :: Python :: 3.7',
     ],
+    include_package_data=True
 )
