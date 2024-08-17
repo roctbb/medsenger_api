7bdf2caa7bbaf01c7e6136d8de27fba18680ee34
roctbb <roctbb@gmail.com>
2024-08-17 18:42:20 +0300

    0.1.72 add certificate for grpc

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='medsenger_api',
-    version='0.1.70',
+    version='0.1.72',
     description='Python SDK for Medsenger.AI',
     url='https://github.com/roctbb/medsenger_api',
     author='Rostislav Borodin',
